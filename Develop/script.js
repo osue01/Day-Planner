f20a2fc4ce9ab6ec76e4c09ccf4a4dfe52c76623
Olivia <osue01@gmail.com>
2024-04-21 13:57:41 -1000

    adds current day and formats scheduler

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,9 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(document).ready(function () {
   const currentHour = dayjs().hour();
   //add event listener
   $("#saveBtn").on("click", function () {
     console.log("Added to plate");
   });
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   // Loop through each time block
   $(".time-block").each(function () {
@@ -51,9 +35,19 @@ $(document).ready(function () {
   });
 
   // TODO: Add code to display the current date in the header of the page.
+
   $(document).ready(function () {
     const currentDate = dayjs().format("MMMM DD, YYYY");
 
     $("#currentDate").text(currentDate);
   });
+  
+
+// const now = new Date();
+// const hourNow = now.getHours();
+// const minutes = now.getMinutes();
+
+// const currentTime = ${hourNow}:${minutes}
+
+
 });
